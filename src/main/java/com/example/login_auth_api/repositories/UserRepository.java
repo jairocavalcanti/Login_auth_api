9cdc2d30219459c8b99427375a60a46c943457a3
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-07-16 19:33:57 -0300

    User findByEmail query implemented

--- src/main/java/com/example/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/example/login_auth_api/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.login_auth_api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.login_auth_api.domainuser.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,String>{
-    
+    Optional<User> findByEmail(String email);
 }
