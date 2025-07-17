c3c42948e28082ea2e7ada627a156b30607c6417
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-07-16 21:50:02 -0300

    Inserting new formatting

--- src/main/java/com/example/login_auth_api/infra/security/TokenService.java
+++ src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -1,4 +1,5 @@
 package com.example.login_auth_api.infra.security;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -15,52 +16,52 @@ import com.example.login_auth_api.domainuser.User;
 public class TokenService {
 
     // "${api.security.token.secret}" in application.properties
-    // The "secret" string variable above holds the value assigned from "${api.security.token.secret}".
+    // The "secret" string variable above holds the value assigned from
+    // "${api.security.token.secret}".
     @Value("${api.security.token.secret}")
     private String secret;
-    
 
     // String method that creates a token for the user passed in the arguments.
     public String generateToken(User user) {
         try {
 
-            // Creating signed algorithm HMAC with secret key 
+            // Creating signed algorithm HMAC with secret key
             Algorithm algorithm = Algorithm.HMAC256(secret);
-
 
             // Creating JWT token
             String token = JWT.create()
-                // token issuer identification
-                .withIssuer("login-auth-api")
-                // token unique identifier
-                .withSubject(user.getEmail())
-                .withExpiresAt(this.generateExpirationDate())
-                // finally apply the digital signature with the secret key and algorithm
-                .sign(algorithm);
+                    // token issuer identification
+                    .withIssuer("login-auth-api")
+                    // token unique identifier
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationDate())
+                    // finally apply the digital signature with the secret key and algorithm
+                    .sign(algorithm);
             return token;
-        } catch (JWTCreationException exception){
+        } catch (JWTCreationException exception) {
             throw new RuntimeException("Error while authenticating");
         }
     }
-        //Validating token
-    public String validateToken(String token){
-       try {
-        Algorithm algorithm = Algorithm.HMAC256(secret);
-            //token verification
+
+    // Validating token
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            // token verification
             return JWT.require(algorithm)
-                   .withIssuer("login auth api")
-                   .build()
-                   .verify(token)
-                   .getSubject();
-            //JWT exception for token
-       } catch (JWTCreationException exception) {
-          return null;
-       } 
+                    .withIssuer("login auth api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+            // JWT exception for token
+        } catch (JWTCreationException exception) {
+            return null;
+        }
     }
 
-    private Instant generateExpirationDate(){
+    private Instant generateExpirationDate() {
         return LocalDateTime.now()
-        .plusHours(2)
-        .toInstant(ZoneOffset.of("-3"));
+                .plusHours(2)
+                .toInstant(ZoneOffset.of("-3"));
     }
 }
