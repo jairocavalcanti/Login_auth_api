36b3edeca28692226503abb90accd4abe48cff01
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-07-21 04:14:37 -0300

    New DTO's (data transfer objects):
      - LoginRequestDTO: email and password
      - RegisterRequestDTO: name, email and password
      - ResponseDTO: name and token

---
+++ src/main/java/com/example/login_auth_api/DTO/LoginRequestDTO.java
@@ -0,0 +1,5 @@
+package com.example.login_auth_api.DTO;
+
+public record LoginRequestDTO(String email, String password) {
+    
+}
---
+++ src/main/java/com/example/login_auth_api/DTO/ResponseDTO.java
@@ -0,0 +1,5 @@
+package com.example.login_auth_api.DTO;
+
+public record ResponseDTO(String name, String token) {
+    
+}
