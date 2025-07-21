f6bf18b2d6c89d8670c7daf9d174b3cc76023c4f
jairocavalcanti <jairoregiscavalcanti@gmail.com>
2025-07-21 04:08:29 -0300

    new annotations in securityFilterChain method

--- src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
+++ src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.loginauthapi.infra.security;
+package com.example.login_auth_api.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +25,7 @@ public class SecurityConfig {
     @Autowired
     SecurityFilter securityFilter;
 
+    // @Bean indicates that the method creates an object managed by Spring
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -32,10 +33,18 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        // the routes below are allowed without user verification
+                        .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/user").permitAll()
+                        // any other request must be authenticated
+                        // otherwise a 403 Forbidden will be thrown
                         .anyRequest().authenticated()
                 )
+                // before authorizing the HTTP request, the security filter will be added 
+                // this filter is defined in the securityFilter class
+                // the filter is necessary to forward the request to the controller layer
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
